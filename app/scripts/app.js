eae40662f7aed2c2b50fc0299035b7da4a594727
Alexander Chernyh <alexchernyh2000@mail.ru>
2024-09-23 23:48:17 +0300

    first commit

--- app/scripts/app.js
+++ app/scripts/app.js
@@ -1,6 +1,24 @@
-import svg4everybody from 'svg4everybody';
 import $ from 'jquery';
 
 $(() => {
-	svg4everybody();
+	$('.main__blur-button').click(function() {
+        const button = $('.main__blur-button');
+        const popup = $('.main__popup');
+        const bt = $('.main__blur-button span');
+        const buttonText = button.text();
+
+        navigator.clipboard.writeText(buttonText).then(() => {
+            console.log('Текст успешно скопирован в буфер обмена');
+        }).catch(err => {
+            console.error('Не удалось скопировать текст: ', err);
+        });
+
+        bt.css('filter', 'none');
+
+        popup.fadeIn();
+
+        setTimeout(function() {
+            popup.fadeOut();
+        }, 4000);
+    });
 });
